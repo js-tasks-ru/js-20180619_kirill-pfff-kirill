7d907cdef7a764dc94cd316623ecbd674a813c4e
krgolovan <krgolovan@gmail.com>
2018-07-10 12:26:34 -0700

    3-module code

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,6 +4,33 @@ let calendar = {
 };
 
 calendar[Symbol.iterator] = function () {
+  let from = this.from;
+  let to = this.to;
+  from.setHours(0,0,0,0);
+  to.setHours(0,0,0,0);
+
+  function incDate(date){
+    date.setDate(from.getDate() + 1);
+
+    let day = date.getDay();
+    let dom = date.getDate();
+
+    if (dom < 10){ dom = '0' + dom; };
+
+    if (day < 6 && day !== 0){
+      return `${dom}`;
+    }else{
+      return `[${dom}]`;
+    }
+  }
+
+  return {
+    next(){
+      if (from.getTime() !== to.getTime()){
+        return {done: false, value: incDate(from)};
+      }else{
+        return {done: true};
+      }
+    }
+  }
 };
-
-
