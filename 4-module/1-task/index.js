3616ae774565d7bf105489c0b83d952ab834f157
krgolovan <krgolovan@gmail.com>
2018-07-12 14:24:12 -0700

    Module 4 code

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -5,5 +5,15 @@
  * @param {Friend[]} friends
  * @return {HTMLUListElement}
  */
+
 function makeFriendsList (friends) {
+    let ul = document.createElement('ul');
+
+    friends.map((item) => {
+        let li = document.createElement('li');
+        li.innerHTML = `${item.firstName} ${item.lastName}`;
+        ul.appendChild(li);
+    });
+
+    return ul;
 }
