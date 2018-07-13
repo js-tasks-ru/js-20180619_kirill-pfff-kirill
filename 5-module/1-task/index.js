ceb5c3707164de873fb1069d9bcde073f6e38c2a
krgolovan <krgolovan@gmail.com>
2018-07-13 15:43:15 -0700

    Module 5 code

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -5,4 +5,34 @@
  * @param {Element} table
  */
 function highlight(table) {
+    const status = table.querySelectorAll('tbody > tr > td:last-child');
+
+    for (let i of status){
+        if (i.dataset.available === 'true'){
+            i.classList.add('available');
+        }else if (i.dataset.available === 'false') {
+            i.classList.add('unavailable');
+        }else{
+            i.hidden = true;
+        }
+    }
+
+    const gender = table.querySelectorAll('tbody > tr > td:nth-child(3)');
+
+    for (let i of gender){
+        if (i.innerHTML === 'm'){
+            i.classList.add('male');
+        }else{
+            i.classList.add('female');
+        }
+    }
+
+    const age = table.querySelectorAll('tbody > tr > td:nth-child(2)');
+
+    for (let i of age){
+        if (parseInt(i.innerHTML) < 18){
+            i.style.textDecoration = 'line-through';
+        }
+    }
+
 }
