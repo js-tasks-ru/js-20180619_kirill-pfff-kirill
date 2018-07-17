7d33daf67969b2b91059341b682240125d43edc9
krgolovan <krgolovan@gmail.com>
2018-07-16 22:51:38 -0700

    Module 6 code

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -5,6 +5,26 @@
  * @param {Date} when - дата события
  * @return {string} - строка с указанием времени до начала события
  */
-function getBeforeTime(when) {
-    let current = Date.now(); // обязательно получать текущую дату через эту функцию. Иначе тесты работать не будут
-}
+ function getBeforeTime(when) {
+     let current = Date.now();
+
+     let diff = new Date(when.getTime() - current);
+
+     if (+diff < 0){
+       return 'событие завершилось';
+     }else{
+       let result = '';
+
+       if (diff.getUTCFullYear() > 1970){
+         result += `${diff.getUTCFullYear() - 1970} г., `
+       }
+       if (diff.getUTCMonth() > 0){
+         result += `${diff.getUTCMonth()} м., `
+       }
+       if (diff.getUTCDate() > 1){
+         result += `${diff.getUTCDate()} д., `
+       }
+
+       return result;
+     }
+ }
